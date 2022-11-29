596042d23bb2a582ef8051a1d6f59b44e25e7009
Raghu Anand <raghuaanand@gmail.com>
2022-11-29 06:03:58 +0530

    File Deleted
    This file was deleted because another file with name "1_Comments.js" was created to follow the sequence.

--- Basic JavaScript/Comments.js
+++
@@ -1,8 +0,0 @@
-/*
-block which starts with forward slash followed by * and ends with
-* followed by forward slash is for multi-line comments.
-*/
-
-// Two forward slash is for single line comments.
-
-// All the best for your journey in learning JavaScript.
