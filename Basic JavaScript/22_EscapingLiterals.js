03a0e730b359294391bd3256cfcf8ea2b9fe855e
Raghu Anand <raghuaanand@gmail.com>
2022-12-06 18:47:47 +0530

    initial commit

---
+++ Basic JavaScript/22_EscapingLiterals.js
@@ -0,0 +1,9 @@
+/*
+
+A string is contained within a pair of single or double quotes. But what if you want to add a single or double quote in that string literal?
+Here we will be helped by the escape literal.
+  --> Just place a backward slash before any single or double qoute you want to escape
+*/
+
+var string1 = "\" I am Raghu Anand. \" ";
+console.log(string1);
